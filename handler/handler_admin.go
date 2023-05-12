8247e95b7cd346048efc461a3c89ee857a93f8ab
Guangzong Chen <chen-gz@outlook.com>
2023-05-12 14:39:42 -0400

    update insert

--- handler/handler_admin.go
+++ handler/handler_admin.go
@@ -5,6 +5,7 @@ import (
 	db "go_blog/database"
 	rd "go_blog/render"
 	"net/http"
+	"time"
 )
 
 func V1UserGetPost(c *gin.Context, url string, userEmail string) {
@@ -53,4 +54,24 @@ func V1UserInsertPost(c *gin.Context, blogData db.BlogDataV1, userToken string) 
 	} else {
 		c.JSON(http.StatusForbidden, gin.H{"error": "permission denied"})
 	}
+}
+
+func V1NewPost(c *gin.Context) {
+	post := db.BlogDataV1{
+		Url: time.Now().String(),
+	}
+	// if this get error, generate new url and try again
+	tries := 0
+	for db.V1InsertPost(post) != nil {
+		tries++
+		post.Url = time.Now().String()
+		if tries > 10 {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to generate url",
+			})
+			return
+		}
+	}
+	post = db.V1GetPostByUrl(post.Url)
+	c.JSON(http.StatusOK, post)
 }
