397ecbc30941dd37405d3ebe5d97378e0a54bfed
Guangzong Chen <chen-gz@outlook.com>
2023-10-19 15:47:09 -0400

    update postpage and photolist

--- front_end/src/views/photoList.vue
+++ front_end/src/views/photoList.vue
@@ -16,11 +16,11 @@ var page = ref(1);
 // }
 
 // function toggleSelect(photo: PhotoWithUrl) {
-    // if (isSelected(photo)) {
-    //     selectedPhotos.value = selectedPhotos.value.filter((element) => element !== photo);
-    // } else {
-    //     selectedPhotos.value.push(photo);
-    // }
+// if (isSelected(photo)) {
+//     selectedPhotos.value = selectedPhotos.value.filter((element) => element !== photo);
+// } else {
+//     selectedPhotos.value.push(photo);
+// }
 // }
 
 var selectMode = ref(false);
@@ -31,83 +31,84 @@ var selectMode = ref(false);
 // }
 
 function closeDialog() {
-    dialog.value = false;
+  dialog.value = false;
 }
 
 function openDialog(photo: PhotoWithUrl) {
-    console.log(photo.jpg_url)
-    dialogImageSrc.value = photo.jpg_url;
-    dialog.value = true;
+  console.log(photo.jpg_url)
+  dialogImageSrc.value = photo.jpg_url;
+  dialog.value = true;
 }
 
 // console.log("fileList: " + fileList)
 fileUploadArea.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    fileUploadArea.classList.add("dragover");
+  event.preventDefault();
+  fileUploadArea.classList.add("dragover");
 });
 fileUploadArea.addEventListener("dragleave", () => {
-    fileUploadArea.classList.remove("dragover");
+  fileUploadArea.classList.remove("dragover");
 });
 fileUploadArea.addEventListener("drop", async (event) => {
-    event.preventDefault();
-    event.stopPropagation(); // Stop event propagation
-    fileUploadArea.classList.remove("dragover");
-    // @ts-ignore
-    const files = event.dataTransfer.files;
-    uploadPhotos(files);
+  event.preventDefault();
+  event.stopPropagation(); // Stop event propagation
+  fileUploadArea.classList.remove("dragover");
+  // @ts-ignore
+  const files = event.dataTransfer.files;
+  uploadPhotos(files);
 });
 // get photo 6
 var showElements = ref([] as PhotoWithUrl[]);
 
 interface PhotoWithUrl {
-    photo: PhotoItemV2;
-    thum_url: string;
-    jpg_url: string;
+  photo: PhotoItemV2;
+  thum_url: string;
+  jpg_url: string;
 }
+
 //
 const number_of_pages = ref(0);
 const number_of_photos = ref(0);
 const page_size = 24;
 
 async function fetchPage(page: number) {
-    console.log("fetch page: " + page)
-    let photoIds = (await getPhotoIds()).ids;
-    // let photoIds = []
-    // generate a list from 1 to 100
-    for (let i = 4528; i <= 4530; i++) {
-        photoIds.push(i)
-    }
-    // sort by id
-    photoIds.sort((a, b) => b - a);
-    // get id base on page and page size
-    const start = (page - 1) * page_size;
-    const end = page * page_size;
-    photoIds = photoIds.slice(start, end);
+  console.log("fetch page: " + page)
+  let photoIds = (await getPhotoIds()).ids;
+  // let photoIds = []
+  // generate a list from 1 to 100
+  for (let i = 4528; i <= 4530; i++) {
+    photoIds.push(i)
+  }
+  // sort by id
+  photoIds.sort((a, b) => b - a);
+  // get id base on page and page size
+  const start = (page - 1) * page_size;
+  const end = page * page_size;
+  photoIds = photoIds.slice(start, end);
 
-    const promises = photoIds.map(async (id) => {
-        // return await getPhoto({"id": id});
-        return await getPhotoById(id)
-    });
-    const photos = await Promise.all(promises);
-    photos.sort((a, b) => b.photo.id - a.photo.id);
-    console.log("photos: " + photos[0].photo.id);
-    showElements.value = [] as PhotoWithUrl[];
-    for (const photo of photos) {
-        showElements.value.push({photo: photo.photo, thum_url: photo.thum_url, jpg_url: photo.jpeg_url});
-    }
+  const promises = photoIds.map(async (id) => {
+    // return await getPhoto({"id": id});
+    return await getPhotoById(id)
+  });
+  const photos = await Promise.all(promises);
+  photos.sort((a, b) => b.photo.id - a.photo.id);
+  console.log("photos: " + photos[0].photo.id);
+  showElements.value = [] as PhotoWithUrl[];
+  for (const photo of photos) {
+    showElements.value.push({photo: photo.photo, thum_url: photo.thum_url, jpg_url: photo.jpeg_url});
+  }
 }
 
 async function fetchData() {
-    const photoIds = (await getPhotoIds()).ids;
-    // let photoIds = []
-    // generate a list from 1 to 100
-    // for (let i = 4528; i <= 4530; i++) {
-    //     photoIds.push(i)
-    // }
-    number_of_photos.value = photoIds.length;
-    number_of_pages.value = Math.ceil(photoIds.length / page_size);
-    console.log("number of pages: " + number_of_pages.value);
-    console.log("number of photos: " + number_of_photos.value);
+  const photoIds = (await getPhotoIds()).ids;
+  // let photoIds = []
+  // generate a list from 1 to 100
+  // for (let i = 4528; i <= 4530; i++) {
+  //     photoIds.push(i)
+  // }
+  number_of_photos.value = photoIds.length;
+  number_of_pages.value = Math.ceil(photoIds.length / page_size);
+  console.log("number of pages: " + number_of_pages.value);
+  console.log("number of photos: " + number_of_photos.value);
 }
 
 fetchData();
@@ -121,53 +122,60 @@ fetchPage(1);
 // }
 
 function downloadPhoto(photo: PhotoWithUrl) {
-    window.open(photo.jpg_url, "_blank");
+  window.open(photo.jpg_url, "_blank");
 }
 
 function sharePhoto(photo: PhotoWithUrl) {
-    // copy to clipboard
-    navigator.clipboard.writeText(photo.jpg_url).then(() => {
-        showSuccess("Copied to clipboard")
-    });
+  // copy to clipboard
+  navigator.clipboard.writeText(photo.jpg_url).then(() => {
+    showSuccess("Copied to clipboard")
+  });
 
 }
 </script>
 <style scoped>
 .select-icon-container {
-    position: absolute;
-    top: 10px; /* Adjust the top position as needed */
-    right: 10px; /* Adjust the right position as needed */
-    z-index: 1; /* Ensure the icon is above the image */
-    opacity: 0.1; /* Adjust the opacity value as needed (0.0 for fully transparent, 1.0 for fully opaque) */
+  position: absolute;
+  top: 10px; /* Adjust the top position as needed */
+  right: 10px; /* Adjust the right position as needed */
+  z-index: 1; /* Ensure the icon is above the image */
+  opacity: 0.1; /* Adjust the opacity value as needed (0.0 for fully transparent, 1.0 for fully opaque) */
 
 }
 
 
 </style>
 <template>
-    <div></div>
+  <div></div>
   <v-container fluid>
-    <v-row>
+    <!--    <v-row>-->
+    <div class="d-flex flex-wrap">
+
       <template v-for="(element, index) in showElements" :key="index">
-        <v-col sm="12" md="3" lg="2">
-          <v-card :key="element.photo.id" >
+<!--        <v-col sm="12" md="3" lg="2">-->
+        <div class="flex-wrap flex-grow-1 d-flex">
+<!--          <v-card :key="element.photo.id" >-->
             <v-img :src="element.thum_url"
                    :key="element.photo.id"
-                   style="position: relative;"
+                   style="height: 40vh"
                    @click="openDialog(element)"
+                   width="auto"
+                   class="fill-height flex-grow-1"
             >
             </v-img>
-            <v-card-actions style="height: 30px;">
-              <v-spacer/>
-              <v-btn icon="mdi mdi-download" @click="downloadPhoto(element)"></v-btn>
-<!--              <v-btn icon="mdi mdi-delete" @click="deletePhoto(element.photo)"></v-btn>-->
-              <v-btn icon="mdi mdi-share-variant" @click="sharePhoto(element)"></v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+<!--            <v-card-actions>-->
+<!--              <v-spacer/>-->
+<!--              <v-btn icon="mdi mdi-download" @click="downloadPhoto(element)"></v-btn>-->
+<!--&lt;!&ndash;              <v-btn icon="mdi mdi-delete" @click="deletePhoto(element.photo)"></v-btn>&ndash;&gt;-->
+<!--              <v-btn icon="mdi mdi-share-variant" @click="sharePhoto(element)"></v-btn>-->
+<!--            </v-card-actions>-->
+<!--          </v-card>-->
+        </div>
+<!--        </v-col>-->
       </template>
 
-    </v-row>
+<!--    </v-row>-->
+      </div>
     <v-dialog v-model="dialog">
       <v-card height=95vh>
         <v-card-title>Dialog Title</v-card-title>
